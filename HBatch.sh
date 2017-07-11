dd7aec4fafb8d4ea69a352b88591bffd0d6ae8d5
Jaska8 <jaska@finbits.net>
2017-07-11 20:14:05 +0300

    Captions just in case...

--- HBatch.sh
+++ HBatch.sh
@@ -7,16 +7,16 @@
 #notes           :Requires HandBrakeCLI to be installed system wide or edit script for HandBrakeCLI full path
 
 #Take user input
-echo Please type the input folder to be converted. Example /media/videos
+echo "Please type the input folder to be converted. Example /media/videos"
 read SRC
 
-echo Please type output folder for converted videos Example /media/output
+echo "Please type output folder for converted videos Example /media/output"
 read DST
 
-echo Please type output extension. mkv or mp4
+echo "Please type output extension. mkv or mp4"
 read EXT
 
-echo Please type in encoding settings. Leave empty for HandBrake defaults. List of settings https://handbrake.fr/docs/en/latest/cli/cli-guide.html
+echo "Please type in encoding settings. Leave empty for HandBrake defaults. List of settings https://handbrake.fr/docs/en/latest/cli/cli-guide.html"
 read ENC_SETTINGS
 
 #List all files inside source folder and remove the file extension for output
